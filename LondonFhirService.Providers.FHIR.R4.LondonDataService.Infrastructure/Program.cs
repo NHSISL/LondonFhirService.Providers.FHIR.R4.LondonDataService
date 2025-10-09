5453798206f107658dc47cbf2e2d34181797613e
Christo du Toit <christo@dutoit.co.uk>
2025-10-09 17:10:57 +0100

    INFRA: Initial Checkin

---
+++ LondonFhirService.Providers.FHIR.R4.LondonDataService.Infrastructure/Program.cs
@@ -0,0 +1,23 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using LondonFhirService.Providers.FHIR.R4.LondonDataService.Infrastructure.Services;
+
+namespace LondonFhirService.Providers.FHIR.R4.LondonDataService.Infrastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+
+            scriptGenerationService.GenerateBuildScript(
+                branchName: "main",
+                projectName: "LondonFhirService.Providers.FHIR.R4.LondonDataService",
+                dotNetVersion: "9.0.100");
+
+            scriptGenerationService.GeneratePrLintScript(branchName: "main");
+        }
+    }
+}
