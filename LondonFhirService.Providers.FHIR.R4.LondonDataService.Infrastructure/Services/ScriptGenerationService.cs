3cadaf026c7f0e5c5b49077141d3359fa03f2c78
Christo du Toit <christo@dutoit.co.uk>
2025-11-06 11:23:39 +0000

    CODE RUB: Updated build script and solution file

--- LondonFhirService.Providers.FHIR.R4.LondonDataService.Infrastructure/Services/ScriptGenerationService.cs
+++ LondonFhirService.Providers.FHIR.R4.LondonDataService.Infrastructure/Services/ScriptGenerationService.cs
@@ -46,6 +46,12 @@ namespace LondonFhirService.Providers.FHIR.R4.LondonDataService.Infrastructure.S
 
                             Steps = new List<GithubTask>
                             {
+                                new GithubTask
+                                {
+                                    Name = "Enable long paths for Git",
+                                    Run = "git config --system core.longpaths true"
+                                },
+
                                 new CheckoutTaskV3
                                 {
                                     Name = "Check out"
